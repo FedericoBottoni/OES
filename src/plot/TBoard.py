41ddcf9a5adb5eaa9f49c50ee19fcce6fb34343c
federicobottoni2 <feder.bottoni@gmail.com>
2020-11-15 13:11:06 +0000

    refactoring

--- src/plot/TBoard.py
+++ src/plot/TBoard.py
@@ -15,8 +15,6 @@ class TBoard(object):
         self._n_cm_reward = 0
         self._n_cm_reward_ep = 0
         self._n_episode_len = 0
-        self._q_value_mean = 0
-        self._q_value_var = 0
         self._sending_data = 0
         self._receiving_data = 0
 
@@ -77,16 +75,7 @@ class TBoard(object):
         self.push_episode_len(dataset.mean())
 
 
-
     # Multi charts
-    def push_q_value_mean_dict(self, my_dict):
-        self.push_scalars_dict("Reward/Q-Value Mean", self._q_value_mean, my_dict)
-        self._q_value_mean += 1
-    
-    def push_q_value_var_dict(self, my_dict):
-        self.push_scalars_dict("Reward/Q-Value Var", self._q_value_var, my_dict)
-        self._q_value_var += 1
-    
     def push_sending_dict(self, my_dict):
         self.push_scalars_dict("Transfer/Sending", self._sending_data, my_dict)
         self._sending_data += 1
@@ -94,17 +83,3 @@ class TBoard(object):
     def push_receiving_dict(self, my_dict):
         self.push_scalars_dict("Transfer/Receiving", self._receiving_data, my_dict)
         self._receiving_data += 1
-
-    def push_q_values(self, action_dict, state_action_values, action_batch):
-        q_values = state_action_values.view(1, -1)[0]
-        q_labels = action_batch.view(1, -1)[0]
-        single_action_labels = torch.unique(q_labels)
-        single_action_values_mean = {}
-        single_action_values_var = {}
-        for action_lbl in single_action_labels:
-            a_mask = q_labels == action_lbl
-            a_q_values = q_values[torch.nonzero(a_mask, as_tuple=True)].view(1, -1)[0]
-            single_action_values_mean[action_dict[str(action_lbl.item())]] = a_q_values.mean().item()
-            single_action_values_var[action_dict[str(action_lbl.item())]] = a_q_values.var().item()
-        self.push_q_value_mean_dict(single_action_values_mean)
-        self.push_q_value_var_dict(single_action_values_var)
